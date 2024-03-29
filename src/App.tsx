cf6f7b4a33c2f5ed10334d0f2bebc400d710a2a6
Jack Treadwell <treadwell.jack@gmail.com>
2024-03-28 21:10:56 -0500

    Change body block transition method to fades instead of slides. Implement sequential fading in and out so visuals dont overlap.

--- src/App.tsx
+++ src/App.tsx
@@ -12,12 +12,21 @@ import { ISectionData } from './datamodel/SectionData'
 function App() {
   const pageData = new FullPageData();
   const [activeBodyBlockIndex, setActiveBodyBlock] = useState(0);
+  const [nextBodyBlock, setNextBodyBlock] = useState(-1);
 
   //handler for when a button is clicked in the sidebar. Changes the active body block
   const sidebarButtonSelectionCallback = (index: number) =>{
     if (index !== activeBodyBlockIndex) {
-      setActiveBodyBlock(index);
+      //when a button is pressed, first tell the current body block to fade out
+      setActiveBodyBlock(-1);
+      //then set the next body block to be the one that was clicked
+      setNextBodyBlock(index); 
     }
+  }
+
+  //handler for when the body block finishes fading out. Changes the active body block to the next body block
+  const bodyBlockFadeOutCallback = () =>{
+    setActiveBodyBlock(nextBodyBlock);
   }
   return (
     <>
@@ -28,7 +37,7 @@ function App() {
           </Col>
           <Col>
             {pageData.getSectionDataArray().map((sectionData: ISectionData, index: number) =>{
-              return <BodyBlock sectionData={sectionData} visible={index === activeBodyBlockIndex}></BodyBlock>
+              return <BodyBlock sectionData={sectionData} visible={index === activeBodyBlockIndex} afterFadeOut={bodyBlockFadeOutCallback}></BodyBlock>
             })}
             
           </Col>
--- src/components/Body/BodyBlock.tsx
+++ src/components/Body/BodyBlock.tsx
@@ -5,32 +5,31 @@
 import Image from 'react-bootstrap/Image'
 import Stack from 'react-bootstrap/Stack'
 import Container from 'react-bootstrap/Container'
-import Collapse from 'react-bootstrap/Collapse'
+import Fade from 'react-bootstrap/Fade'
 import { IBodyTextSection, ISectionData } from "../../datamodel/SectionData";
 
 function BodyBlock(props: IBodyBlockProps){
-    return <>
-    <Collapse in={props.visible}>
-    <Stack  gap={3} className="justify-content-md-center bg-body"  >
-      <Container fluid  style={{paddingTop: '3rem', textAlign: 'center'}}>
-        <Image src={props.sectionData.imagePath} style={{height: '30rem'}} roundedCircle fluid />
-      </Container>
-      <Container fluid>
-        {props.sectionData.bodyTextSections.map((value: IBodyTextSection) =>{
-          return <>
-            <h1>{value.title}</h1>
-            <p>{value.body}</p>
-          </> })}
-      </Container>
-    </Stack>
-    </Collapse>
-    </>;
-
+    return (
+    <Fade in={props.visible} mountOnEnter unmountOnExit onExited={props.afterFadeOut}>
+      <Stack gap={3} className="justify-content-md-center bg-body"  >
+        <Container fluid  style={{paddingTop: '3rem', textAlign: 'center'}}>
+          <Image src={props.sectionData.imagePath} style={{height: '30rem'}} roundedCircle fluid />
+        </Container>
+        <Container fluid>
+          {props.sectionData.bodyTextSections.map((value: IBodyTextSection) =>{
+            return <>
+              <h1>{value.title}</h1>
+              <p>{value.body}</p>
+            </> })}
+        </Container>
+      </Stack>
+    </Fade>);
 }
 
 interface IBodyBlockProps{
   sectionData: ISectionData;
   visible: boolean;
+  afterFadeOut?: () => void;
 }
 
 export default BodyBlock;
