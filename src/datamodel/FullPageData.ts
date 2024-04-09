eb82fdb00fceb64739d041a94a15ce1f7a3f0b6e
Jack Treadwell <treadwell.jack@gmail.com>
2024-04-08 19:51:40 -0500

    Fix image path for My Career page

--- src/datamodel/FullPageData.ts
+++ src/datamodel/FullPageData.ts
@@ -64,7 +64,7 @@ export class FullPageData{
     private __getMyCareerSection(): ISectionData{
         const sectionData: ISectionData = {
             buttonTitle: "My Career",
-            imagePath: '../src/assets/1704245613526.jpg',
+            imagePath: portraitImgUrl,
             bodyTextSections: []
        };
        sectionData.bodyTextSections.push({
